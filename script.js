2196a0c12d4a0b5c46828fe1884dc50e1e309d75
kaserrr <99057099@mydavinci.nl>
2022-01-13 13:58:32 +0100

    Commit #1

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 var input = document.getElementById("input");
-var random = Math.floor(Math.random() * words.length);
-console.log(random);
+var randomWord = words[Math.floor(Math.random() * words.length)];
+var word = randomWord.split("");
+
+
+
+wordBoxes();
+function wordBoxes() {
+	var ul = document.createElement("ul");
+
+	for (i = 0; i < 5; i++) {
+		var li = document.createElement("li");
+		li.innerHTML = "test";
+		ul.appendChild(li);
+	}
+	document.getElementById("letter").appendChild(ul);
+}

c10eaa52a9ef3a9294ecfff28a85b6f6192d5add
kaserrr <99057099@mydavinci.nl>
2022-01-14 10:43:33 +0100

    Commit #6

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 var randomWord = words[Math.floor(Math.random() * words.length)];
 var word = randomWord.split("");
-var input = document.getElementById("input").value;
-var inputWord = input.split("");
-var guess = 0;
-console.log(randomWord);
-console.log(word);
 
+var guess = 0;
 
 wordBoxes();
 function wordBoxes() {
@@ -19,11 +15,13 @@ function wordBoxes() {
 		ul.appendChild(li);
 	}
 	document.getElementById("letter").appendChild(ul);
+	document.getElementById(`letter${0}`).innerHTML = word[0]
 	guess++
 }
 
 function check() {
-	console.log(inputWord)
+	var input = document.getElementById("input").value;
+	var inputWord = input.split("");
 
 	for (var i = 0; i < 5; i++) {
 		if (inputWord[i] == word[i]) {
@@ -36,13 +34,17 @@ function check() {
 			document.getElementById(`letter${i}`).innerHTML = " ";		
 		}
 	}
-	if (inputWord.every(element == word) ) {
+	if (input == randomWord) {
 		var win = true;
 		console.log()
 	    setTimeout(function(){
 	        alert("Je hebt het woord goed geraden!");
 	        location.reload();},1000)
 	}
+
+	for (i = 0; i < 5; i++) {
+        document.getElementById(`letter${i}`).id = "";
+    }
 
 	if (win != true) {
 		wordBoxes();
